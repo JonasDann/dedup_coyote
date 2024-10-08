3b4d72ae8261dfc32d83d9af7539b71181896182
Jonas Dann <jonas.dann@inf.ethz.ch>
2024-10-08 22:34:43 +0200

    Fixed local IP script

--- get_ip_local.sh
+++ get_ip_local.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CLI_PATH=/opt/sgrt/cli
 device_index=1
 
-IP_address_cpu1=$($CLI_PATH/get/ifconfig | awk '$1 == "0:" {print $2}')
+IP_address_cpu1=$($CLI_PATH/get/ifconfig | awk '$1 == "1:" {print $2}')
 IP_address_cpu1_hex=$($CLI_PATH/common/address_to_hex IP $IP_address_cpu1)
 
 IP_address_0=$($CLI_PATH/get/network -d $device_index | awk '$1 == "1:" {print $2}')
